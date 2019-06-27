753ecd9c78f3663779173e0d5c90bdde07ed7517
Alanigma <alanlua2020@hotmai.com>
2019-06-27 10:23:39 -0300

    Consertando insercao de dados do login

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,22 @@
 |
 */
 
-Auth::routes();
+//Auth::routes();
+//Deixar isso no lugar do Auth::routes() para poder alterar o nome das rotas
+// Authentication Routes...
+$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+$this->post('login', 'Auth\LoginController@login');
+$this->post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+$this->get('register', 'Auth\RegisterController@getRegisterForm')->name('register');
+$this->post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+$this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+$this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+$this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+$this->post('password/reset', 'Auth\ResetPasswordController@reset');
 
 Route::group( ['middleware' => 'auth'], function()
 {
