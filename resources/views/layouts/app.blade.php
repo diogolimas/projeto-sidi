c652042ed0c1fb8b2bccb991115b0795fbd861ad
Alanigma <alanlua2020@hotmail.com>
2019-07-04 07:58:01 -0300

    Traduzindo Tela de Login e Register

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,11 +45,11 @@
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Entrar') }}</a>
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Registrar') }}</a>
                                 </li>
                             @endif
                         @else
