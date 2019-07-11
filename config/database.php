48a89b0f992b8675223b0eb038eb555b6c798ced
diogolimas <diogo.libras43@gmail.com>
2019-07-11 11:05:29 -0300

    zasfa

--- config/database.php
+++ config/database.php
@@ -48,7 +48,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'navi'),
+            'database' => env('DB_DATABASE', 'navo'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', 'root'),
             'unix_socket' => env('DB_SOCKET', ''),
