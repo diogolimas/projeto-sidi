4b92bec8d0d8795b22f77d567c3941bbc2a4c354
Alanigma <alanlua2020@hotmail.com>
2019-07-11 09:59:11 -0300

    Adicionando dropdown na hora de registrar usuario

--- resources/views/site/home/registrar.blade.php
+++ resources/views/site/home/registrar.blade.php
@@ -7,13 +7,92 @@
 @stop
 
 @section('content')
-    <a href="{{route('registrarAluno')}}" class="ui orange button btn-lg btn-line">
-        Registrar Aluno
-    </a>
-    <a href="{{route('registrarProfessor')}}" class="ui orange button btn-lg btn-line">
-        Registrar Professor
-    </a>
-    <a href="{{route('registrarAdmin')}}" class="ui orange button btn-lg btn-line">
-        Registrar Administrador
-    </a>
+    <div class="ui container">
+
+        <div class="ui internally celled grid">
+
+            <div class="row">
+
+                <div class="three wide column">
+
+                </div>
+
+                <div class="ten wide column">
+                    <div class="d-block mb-4">
+                        <h2 class="ui header ">  <a href="{{url('/')}}">
+                                <i class="arrow left icon black"></i>
+                            </a>
+                            {{$titulo_page ?? 'Registrar Novo Usuário'}}
+                        </h2>
+                    </div>
+                    <form class="" action="{{ route('efetuarRegistro') }}" method="post">
+                        {!! csrf_field() !!}
+
+                        <div class="ui input mb-2 has-feedback {{ $errors->has('name') ? 'has-error' : '' }} d-block ">
+                            <input type="text" name="name" class="form-control" value="{{ old('name') }}"
+                                   placeholder="{{ trans('adminlte::adminlte.full_name') }}">
+                            <i class="glyphicon glyphicon-user form-control-feedback"></i>
+                            @if ($errors->has('name'))
+                                <span class="help-block">
+                                            <strong>{{ $errors->first('name') }}</strong>
+                                        </span>
+                            @endif
+                        </div>
+                        <div class="ui input d-block mb-2 has-feedback {{ $errors->has('email') ? 'has-error' : '' }}">
+                            <input type="email" name="email" class="form-control" value="{{ old('email') }}"
+                                   placeholder="{{ trans('adminlte::adminlte.email') }}">
+                            <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                            @if ($errors->has('email'))
+                                <span class="help-block">
+                                            <strong>{{ $errors->first('email') }}</strong>
+                                        </span>
+                            @endif
+                        </div>
+                        <div class="ui input d-block mb-2 has-feedback {{ $errors->has('password') ? 'has-error' : '' }}">
+                            <input type="password" name="password" class="form-control"
+                                   placeholder="{{ trans('adminlte::adminlte.password') }}">
+                            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                            @if ($errors->has('password'))
+                                <span class="help-block">
+                                            <strong>{{ $errors->first('password') }}</strong>
+                                        </span>
+                            @endif
+                        </div>
+                        <div class="ui input d-block mb-2 has-feedback {{ $errors->has('password_confirmation') ? 'has-error' : '' }}">
+                            <input type="password" name="password_confirmation" class="form-control"
+                                   placeholder="{{ trans('adminlte::adminlte.retype_password') }}">
+                            <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
+                            @if ($errors->has('password_confirmation'))
+                                <span class="help-block">
+                                            <strong>{{ $errors->first('password_confirmation') }}</strong>
+                                        </span>
+                            @endif
+                        </div>
+                        <select name="papel" class="ui selection dropdown">
+                            <option value="1">Aluno</option>
+                            <option value="2">Professor</option>
+                            <option value="3">Administrador</option>
+                        </select>
+                        <div class="ui input mb-2 has-feedback d-block ">
+                            <input type="text" name="periodo" class="form-control" value="{{ old('periodo') }}"
+                                   placeholder="Período">
+                            <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
+                        </div>
+
+                        <button type="submit"
+                                class="ui inverted orange button btn-lg btn-block"
+                        >{{ trans('adminlte::adminlte.register') }}</button>
+
+                    </form>
+                </div>
+                <!--fim do formulário centralizado-->
+                <div class="three wide column">
+
+                </div>
+            </div>
+            <!--fim da row-->
+        </div>
+
+
+    </div>
 @stop
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@
 //Route::get('/','Admin\AdminController@index')->name('admin');
 Auth::routes();
 Route::group( ['middleware' => 'auth'], function(){
-    Route::resource('/registrar/aluno', 'AlunoController');
-    Route::resource('/registrar/professor', 'ProfessorController');
-    Route::resource('/registrar/admin', 'registroController');
+    Route::resource('/registrar', 'registroController');
     Route::get('/', 'Admin\AdminController@index')->name('home');
     Route::get('/usuarios', 'HomeController@listarUsuario')->name('listarUser');
 
@@ -23,16 +21,9 @@ Route::group( ['middleware' => 'auth'], function(){
     Route::get('/registrar-turmas', 'Turma\TurmaController@index')->name('turmas/registrar');
     Route::post('/registrar-turmas', 'Turma\TurmaController@create')->name('turmas/registrar');
 
-
-    Route::get('/registrar', 'HomeController@registro')->name('registrar');
-
-    Route::get('/registrar/aluno', 'AlunoController@index')->name('registrarAluno');
-    Route::get('/registrar/professor', 'ProfessorController@index')->name('registrarProfessor');
-    Route::get('/registrar/admin', 'registroController@index')->name('registrarAdmin');
-
-    Route::post('/registrar/aluno/efetuar', 'AlunoController@store')->name('RegistrarAluno');
-    Route::post('/registrar/professor/efetuar', 'ProfessorController@store')->name('RegistrarProfessor');
-    Route::post('/registrar/admin/efetuar', 'registroController@store')->name('RegistrarAdmin');
+    //registrar usuario
+    Route::get('/registrar', 'registroController@index')->name('registrar');
+    Route::post('/registrar/efetuar', 'registroController@store')->name('efetuarRegistro');
 });
 
 Route::get('/home', 'HomeController@index')->name('home');
